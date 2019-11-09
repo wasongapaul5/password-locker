1f0adccaa4249b942e8a227430682059a656eace
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-09 13:44:46 +0300

    add test case to test if we can save multiple user objects

--- sec.py
+++ sec.py
@@ -7,8 +7,17 @@ class User:
 
     def __init__ (self,first_name,last_name,phone_number,email,password):
 
+
+
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
         self.email = email
         self.password = password
+
+    def save_user(self):
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self)
--- sec_test.py
+++ sec_test.py
@@ -26,6 +26,25 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.phone_number,"0758669657")
         self.assertEqual(self.new_user.email,"wasongapaul5@gmail.com")
 
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into the user list
+        '''
+
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+    def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user to check if we can save multiple user objects to our user_list
+        '''
+
+        self.new_user.save_user()
+        test_user = User("Paul","Tennison","0703921156","paultennison@gmail.com")
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+
+    
 
 if __name__ == '__main__':
     unittest.main()
