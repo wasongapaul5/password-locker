4c517926ac7aa7a1c19f0becd7ff7be3ee94fe03
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-09 13:21:11 +0300

    check if the objectis instantiated correctly

--- sec_test.py
+++ sec_test.py
@@ -1,2 +1,31 @@
 import unittest
-from user import User
+from sec import User
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase:TestCase class that in creating test cases
+    '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Paul","Wasonga","0758669657","wasongapaul5@gmail.com","1234") 
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name,"Paul")
+        self.assertEqual(self.new_user.last_name,"Wasonga")
+        self.assertEqual(self.new_user.phone_number,"0758669657")
+        self.assertEqual(self.new_user.email,"wasongapaul5@gmail.com")
+
+
+if __name__ == '__main__':
+    unittest.main()
