f8c84cc769c17eb4b8d953c80761ec934a4217dc
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-09 14:08:10 +0300

    create a delete method to delete user from the user list

--- sec_test.py
+++ sec_test.py
@@ -25,6 +25,16 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.last_name,"Wasonga")
         self.assertEqual(self.new_user.phone_number,"0758669657")
         self.assertEqual(self.new_user.email,"wasongapaul5@gmail.com")
+        self.assertEqual(self.new_user.password,"1234")
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each case has run.
+        '''
+
+        User.user_list = []
+
+    
 
     def test_save_user(self):
         '''
@@ -40,11 +50,22 @@ class TestUser(unittest.TestCase):
         '''
 
         self.new_user.save_user()
-        test_user = User("Paul","Tennison","0703921156","paultennison@gmail.com")
+        test_user = User("Paul","Tennison","0703921156","paultennison@gmail.com","1234")
         test_user.save_user()
         self.assertEqual(len(User.user_list),2)
 
-    
+    def test_delete_user(self):
+        '''
+        test_delete_user to test if we can remove a user from our user list
+        '''
+
+        self.new_user.save_user()
+        test_user = User("Paul","Tennison","0703921156","paultennison@gmail.com","1234")
+        test_user.save_user()
+
+        self.new_user.delete_user()
+        self.assertEqual(len(User.user_list),1)        
+        
 
 if __name__ == '__main__':
     unittest.main()
