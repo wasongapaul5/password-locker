ecc19d0007c154475e3cb065c923f90a2113a619
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-09 12:36:04 +0300

    add instances of the class users

--- sec.py
+++ sec.py
@@ -3,4 +3,14 @@ class User:
     Class that generates new instances of users
     '''
 
-pass
+    def __init__ (self,first_name,last_name,phone_number,email,password):
+        '''
+        __init__ method that helps to define properties of our objects.
+
+        Args:
+            first_name:New contact first name.
+            last_name:New contact last name.
+            phone_number:New contact phone number.
+            email:New contact email address.
+            password:New password.
+        '''
