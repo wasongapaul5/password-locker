a6f73f18f3625e6f6ff3f833d510f6741a2ad0b7
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-09 12:39:43 +0300

    create instance variables

--- sec.py
+++ sec.py
@@ -4,13 +4,9 @@ class User:
     '''
 
     def __init__ (self,first_name,last_name,phone_number,email,password):
-        '''
-        __init__ method that helps to define properties of our objects.
 
-        Args:
-            first_name:New contact first name.
-            last_name:New contact last name.
-            phone_number:New contact phone number.
-            email:New contact email address.
-            password:New password.
-        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
+        self.password = password
