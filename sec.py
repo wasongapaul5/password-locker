fa2bb97e3b59b7f23dd147556600f0b434ca433e
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-11 12:21:15 +0300

    add a delete method

--- sec.py
+++ sec.py
@@ -7,13 +7,11 @@ class User:
 
     def __init__ (self,first_name,last_name,phone_number,email,password):
 
-
-
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone_number = phone_number
-        self.email = email
-        self.password = password
+       self.first_name = first_name
+       self.last_name = last_name
+       self.phone_number = phone_number
+       self.email = email
+       self.password = password
 
     def save_user(self):
         '''
@@ -24,47 +22,7 @@ class User:
 
     def delete_user(self):
         '''
-        delete_user method deletes a saved user from the user_list
+        delete_user method deletes a saved contact from our user_list
         '''
 
         User.user_list.remove(self)
-
-       
-    @classmethod
-    def find_by_number(cls,number):
-        '''
-        Method that takes in a number and returns  a user that matches that number.
-
-        Args:
-            number:Phone number to search for
-        Returns:
-            User of person that matches the number.
-        '''
-
-        for user in cls.user_list:
-            if user.phone_number == number:
-                return user
-
-    @classmethod
-    def user_exists(cls,number):
-        '''
-        Method that checks if a user exists from the user list.
-        Args:
-            number:phone number to search if it exists
-        Returns:
-            Boolean:True or false depending if the user exists
-        '''
-
-        for user in cls.user_list:
-            if user.phone_number == number:
-                return True
-
-        return False
-
-    @classmethod
-    def display_users(cls):
-        '''
-        method that returns the user list
-        '''
-
-        return cls.user_list
--- sec_test.py
+++ sec_test.py
@@ -1,25 +1,25 @@
 import unittest
 from sec import User
-import pyperclip
 
 class TestUser(unittest.TestCase):
 
     '''
-    Test class that defines test cases for the user class behaviours.
+    Test  class that defines test cases for the user class behaviours.
 
     Args:
-        unittest.TestCase:TestCase class that in creating test cases
+        unittest.TestCase:TestCase class that helps in creating test cases
     '''
+
     def setUp(self):
         '''
-        Set up method to run before each test cases.
+        set up method to run before each test cases.
         '''
-        self.new_user = User("Paul","Wasonga","0758669657","wasongapaul5@gmail.com","1234") 
 
+        self.new_user = User("Paul","Wasonga","0758669657","wasongapaul5@gmail.com","1234")
 
     def test_init(self):
         '''
-        test_init test case to test if the object is initialized properly
+        test_init test case to test if the object is initialised properly
         '''
 
         self.assertEqual(self.new_user.first_name,"Paul")
@@ -30,12 +30,10 @@ class TestUser(unittest.TestCase):
 
     def tearDown(self):
         '''
-        tearDown method that does clean up after each case has run.
+        tearDown method that does clean up after each test case has run.
         '''
 
         User.user_list = []
-
-    
 
     def test_save_user(self):
         '''
@@ -45,66 +43,39 @@ class TestUser(unittest.TestCase):
         self.new_user.save_user()
         self.assertEqual(len(User.user_list),1)
 
-    def test_save_multiple_user(self):
+    def test_save_multiple_uset(self):
         '''
-        test_save_multiple_user to check if we can save multiple user objects to our user_list
+        test_save_multiple_user to check if we can save multiple user objects to our user list
         '''
 
         self.new_user.save_user()
-        test_user = User("Paul","Tennison","0703921156","paultennison@gmail.com","1234")
+        test_user = User("Oketch","Paul","0703921156","oketchpaul@gmail.com","1111")
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+        
+    def test_save_multiple_contact(self):
+        '''
+        test_save_multiple_contact to check if we can save multiple contact
+        objects to our contact_list
+        '''
+        self.new_user.save_user()
+        test_user = User("Test","user","0712345678","test@user.com","1111")
         test_user.save_user()
         self.assertEqual(len(User.user_list),2)
 
-    def test_delete_user(self):
-        '''
-        test_delete_user to test if we can remove a user from our user list
-        '''
+    def test_delete_contact(self):
+            '''
+            test_delete_contact to test if we can remove a contact from our contact list
+            '''
+            self.new_user.save_user()
+            test_user = User("Test","user","0712345678","test@user.com")
+            test_user.save_user()
 
-        self.new_user.save_user()
-        test_user = User("Paul","Tennison","0703921156","paultennison@gmail.com","1234")
-        test_user.save_user()
-
-        self.new_user.delete_user()
-        self.assertEqual(len(User.user_list),1)     
-
-    def test_find_user_by_number(self):
-        '''
-        test to check if we can find a user by phone number and display information
-        '''
-
-        self.new_user.save_user()
-        test_user = User("Paul","Tennison","0711223344","paultennison@gmail.com","1234")
-        test_user.save_user()
-
-        found_user = User.find_by_number("0711223344")
-        self.assertEqual(found_user.email,test_user.email)
-
-   
-    def test_user_exists(self):
-        '''
-        test to check if we can return a Boolean if we cannot find the contact.
-        '''
-
-        self.new_user.save_user()
-        test_user = User("Paul","Tennison","0711223344","paultennison@gmail.com","1234")
-        test_user.save_user()
-
-        user_exists = User.user_exist("0711223344")
-
-        self.assertTrue(user_exists)
-
-    def test_display_all_users(self):
-        '''
-        method that returns a list of all users saved
-        '''
-
-        self.assertEqual(User.display_users(),User.user_list)
-
-    def test_copy_email(self):
-        '''
-        Test to confirm that we are copying the email address
-         
+            self.new_contact.delete_contact()
+            self.assertEqual(len(User.user_list),1)
         
+
+  
 
 if __name__ == '__main__':
     unittest.main()
